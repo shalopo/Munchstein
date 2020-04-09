52160ebb6e9cda05fc043eb59976fc860cfad9cd
shalo <shalo@DESKTOP-DFOK1EV>
2020-04-09 21:07:51 +0300

    better munch level

--- Levels/Easy/FirstMunchLevel.cs
+++ Levels/Easy/FirstMunchLevel.cs
@@ -10,33 +10,35 @@ namespace Munchstein.Levels.Easy
     {
         protected override void Build()
         {
-            Add(Platform.Concrete(new Point2(2, 3), width: 4));
+            Add(Platform.Concrete(new Point2(4, 3), width: 2));
             Add(Platform.Concrete(new Point2(9, 3), width: 2));
-            Add(Platform.Concrete(new Point2(13.5, 3), width: 1));
             Add(Platform.OneWay(new Point2(7, 7), width: 5.7));
-            Add(Platform.PassThrough(new Point2(16.3, 8), width: 1));
+            Add(Platform.PassThrough(new Point2(16.5, 8), width: 1));
             Add(Platform.OneWay(new Point2(18.2, 5), width: 1.8));
             Add(Platform.Concrete(new Point2(17.5, 3), width: 1));
+            Add(Platform.Concrete(new Point2(19.5, 4), width: 1));
             Add(Platform.OneWay(new Point2(7, 9), width: 14));
             Add(Platform.OneWay(new Point2(14.3, 6), width: 2.6));
-            Add(Platform.Concrete(new Point2(13.7, 12), width: 0.3, height: 2.5));
+            Add(Platform.OneWay(new Point2(20, 8), width: 1));
+            Add(Platform.Concrete(new Point2(8, 12), width: 0.3, height: 1.5));
             Add(Platform.PassThrough(new Point2(10.5, 4), width: 1));
             Add(Platform.PassThrough(new Point2(10.5, 8), width: 2.5));
             Add(Platform.PassThrough(new Point2(20, 7), width: 1));
             Add(Platform.PassThrough(new Point2(13.4, 4), width: 1.5));
-            Add(Platform.Concrete(new Point2(21, 7), width: 3));
-            Add(Platform.Concrete(new Point2(21, 16), width: 1, height: 7));
+            Add(Platform.Concrete(new Point2(21, 8), width: 3));
+            Add(Platform.Concrete(new Point2(21, 17), width: 1, height: 7));
             Add(Platform.PassThrough(new Point2(22, 11), width: 2));
         }
 
         protected override void PostBuild(Level level)
         {
-            level.Munch = new Munch(new Point2(9.5, 3));
+            level.Munch = new Munch(new Point2(19, 5));
+            //level.Munch = new Munch(level.Actor.Location);
             level.Door.Height = 2;
 
             level.Actor.OnSizeUp += () =>
             {
-                LevelContext.DisplayMessage("Big boy now");
+                LevelContext.DisplayMessage("Big boys can jump higher!");
             };
 
             var bigJumps = 0;
@@ -48,12 +50,9 @@ namespace Munchstein.Levels.Easy
                     switch (bigJumps)
                     {
                         case 0:
-                            LevelContext.DisplayMessage("You can jump higher now");
-                            break;
-                        case 1:
                             LevelContext.DisplayMessage("It's a good thing!");
                             break;
-                        case 2:
+                        case 1:
                             LevelContext.DisplayMessage("Right?...");
                             break;
                         default:
